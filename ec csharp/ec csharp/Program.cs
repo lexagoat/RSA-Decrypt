813dcf9b5ad6d75b7f5ce918b2966863de604192
lexa <76516963+lyrexdrexy@users.noreply.github.com>
2022-04-27 22:15:38 +0300

    Add files via upload

---
+++ ec csharp/ec csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+public string Decrypt(string Text)
+{
+    string BOS = "";
+    try
+    {
+        var privateKey = "aE7f03zE"; // secret key 
+        var testDate = Encoding.UTF8.GetBytes(Text);
+        using (var rsa = new RSACryptoServiceProvider(1024))
+        {
+            try
+            {
+                var base64Encrypted = Text;
+                rsa.FromXmlString(privateKey);
+                var resultBytes = Convert.FromBase64String(base64Encrypted);
+                var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                return decryptedData.ToString();
+            }
+            finally
+            {
+                rsa.PersistKeyInCsp = false;
+            }
+        }
+
+    }
+    catch { return BOS; }
+}
